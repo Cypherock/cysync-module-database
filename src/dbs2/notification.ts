688177ca0617a3d5e1e90f9eaa23267c7727334b
Harish <harish@cypherock.com>
2022-05-12 19:46:09 +0530

    add and configure pouchDB

--- src/dbs2/notification.ts
+++ src/dbs2/notification.ts
@@ -1,37 +1,52 @@
-import { Db } from '../module2/database';
+import { Db } from '../module2/database2';
 import Notification from '../models2/notification';
 
 export class NotificationDb extends Db<Notification> {
   constructor() {
     super('notification');
-    this.executeSql(`CREATE TABLE IF NOT EXISTS ${this.table} (
-            _id TEXT NOT NULL,
-            title TEXT NOT NULL,
-            description TEXT,
-            type INTEGER NOT NULL,
-            createdAt DATETIME NOT NULL,
-            updatedAt DATETIME NOT NULL,
-            isRead BOOLEAN NOT NULL,
-            PRIMARY KEY (_id)
-        )`);
   }
 
-  public async getLast(): Promise<Notification | null> {
-    const data = await this.getAll(
-      {},
-      {
-        sort: '_id',
-        order: 'desc',
-        limit: 1
-      }
-    );
-    if (data.length > 0) {
-      return data[0];
+  public async getAll(perPageLimit = 3) {
+    const res = await this.db.find({
+      selector: {
+        _id: { $gte: null }
+      },
+      sort: [{ _id: 'desc' }],
+      limit: perPageLimit
+    })
+
+    return res.docs;
+  }
+
+  public async getNext(_id: string, perPageLimit = 3) {
+    const res = await this.db.find({
+      limit: perPageLimit,
+      selector: {
+        _id: { $lte: _id }
+      },
+      sort: [{ _id: 'desc' }]
+    });
+    return res.docs;
+  }
+
+  public async getLastId(): Promise<Notification | null> {
+    const res = await this.db.find({
+      selector: {
+        _id: { $gte: null }
+      },
+      fields: ['_id'],
+      sort: [{ _id: 'desc' }],
+      limit: 1
+    });
+    if (res.docs.length === 0) {
+      return null;
     }
-    return null;
+    return res.docs[0];
   }
 
   public async markAllAsRead() {
-    await this.update({ isRead: true }, { isRead: false });
+    this.db.query(doc => {
+      doc.isRead = true;
+    });
   }
 }
---
+++ src/module2/database2.ts
@@ -0,0 +1,30 @@
+import { EventEmitter } from 'events';
+import { PassEncrypt } from '../dbs2';
+// import logger from '../utils/logger';
+import PouchDB from 'pouchdb';
+import PouchDBWebSQLAdapter from 'pouchdb-adapter-websql';
+import PouchFind from 'pouchdb-find';
+PouchDB.plugin(PouchDBWebSQLAdapter);
+PouchDB.plugin(PouchFind);
+
+export abstract class Db<T> {
+  public table: string;
+  public db: PouchDB.Database<T>;
+  public emitter = new EventEmitter();
+  protected refEnDb: PassEncrypt | undefined;
+
+  constructor(table: string, enDb?: PassEncrypt) {
+    this.table = table;
+    this.refEnDb = enDb;
+    this.db = new PouchDB<T>(table, {adapter: 'websql'});
+  }
+
+  /**
+   * emits an event
+   * @param event - the event to emit.
+   * @param payload - the payload to emit. can be anything, boolean, object, string
+   */
+  public emit(event: string, payload?: any) {
+    this.emitter.emit(event, payload);
+  }
+}
