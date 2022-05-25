4e870000161c0ac6e19b17ef95bc94ce68e27afa
Harish <harish@cypherock.com>
2022-05-25 19:01:07 +0530

    implement old notification fetch strategy

--- src/websql/dbs/notification.ts
+++ src/websql/dbs/notification.ts
@@ -10,47 +10,16 @@ export class NotificationDB extends Database<Notification> {
     super('notification', 'v1');
   }
 
-  public async get(perPageLimit = 3) {
+  public async getLatest(items = 3, skip = 0) {
     const res = await this.db.find({
       selector: {
         _id: { $gte: null }
       },
       sort: [{ _id: 'desc' }],
-      limit: perPageLimit
-    });
-
-    return res.docs;
-  }
-
-  public async getNext(_id: string, perPageLimit = 3) {
-    const res = await this.db.find({
-      limit: perPageLimit,
-      selector: {
-        _id: { $lte: _id }
-      },
-      sort: [{ _id: 'desc' }]
+      limit: items,
+      skip
     });
     return res.docs;
-  }
-
-  /**
-   * This function is used to get the last notification in Db. So the client can pass this to
-   * server and fetch all the latest notifications that are not present on client/
-   * @returns {Promise<Notification>}
-   */
-  public async getLastId(): Promise<Notification | null> {
-    const res = await this.db.find({
-      selector: {
-        _id: { $gte: null }
-      },
-      fields: ['_id'],
-      sort: [{ _id: 'desc' }],
-      limit: 1
-    });
-    if (res.docs.length === 0) {
-      return null;
-    }
-    return res.docs[0];
   }
 
   public async markAllAsRead() {
--- src/websql/models/notification.ts
+++ src/websql/models/notification.ts
@@ -3,10 +3,11 @@ export default interface INotification {
    *  unique identifier determined by the server
    */
   _id: string;
+  prevDbId?: string;
+  dbId?: string;
   title: string;
   description?: string;
   type: number;
   createdAt: Date;
-  updatedAt: Date;
   isRead: boolean;
 }
