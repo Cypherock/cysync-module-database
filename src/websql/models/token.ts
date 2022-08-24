8a68fc5f1995793deeffcbc5cfa703c478413b6e
Tejasv Sharma <tejasvonly@gmail.com>
2022-08-24 13:03:37 +0530

    CSI-1003 add priceLastUpdateAt column in coin and tokens

--- src/websql/models/token.ts
+++ src/websql/models/token.ts
@@ -11,5 +11,7 @@ export default interface Token extends IModel {
    */
   slug: string;
   price: number;
+  // stores the timestamp of the last price update in unix epoch time format
+  priceLastUpdatedAt:number;
   balance: string;
 }
